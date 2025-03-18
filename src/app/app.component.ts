9d4634645fa9561878099482b535f67a4fc61136
Nguyen Huynh <nhuynh9497@gmail.com>
2025-03-18 16:58:59 -0400

    setup frontend.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, effect, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScrollingModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FlashCardLearning-Frontend';
+  flashCards: any[] = [];
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get<any[]>('https://localhost:7068/api/FlashCard').subscribe(x=> {
+      this.flashCards = x;
+    });
+  }
 }
