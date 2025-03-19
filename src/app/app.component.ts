90a6776d3b39e2aa1c7727b47543e5d598112784
Averageasd <nhuynh9497@gmail.com>
2025-03-18 22:59:06 -0400

    added infinite loading.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,52 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, effect, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkVirtualScrollViewport, ScrollingModule} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ScrollingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'FlashCardLearning-Frontend';
   flashCards: any[] = [];
-
+  private loading: boolean = false;
+  @ViewChild('flashcardVirtualList') scrollViewport!: CdkVirtualScrollViewport;
   constructor(private http: HttpClient){}
+  ngAfterViewInit(): void {
+    this.loadData();
+  }
+
   ngOnInit(): void {
+
+  }
+
+  private loadData():void {
+    console.log("load data triggered");
+    if (this.loading){
+      return;
+    }
+    this.loading = true;
     this.http.get<any[]>('https://localhost:7068/api/FlashCard').subscribe(x=> {
-      this.flashCards = x;
+      this.flashCards = [...this.flashCards, ...x];
+      console.log(this.flashCards);
+      this.loading = false;
+    }, (error)=>{
+      this.loading = false;
     });
+  }
+
+  onScroll(index: number):void {
+    const end = this.scrollViewport.getRenderedRange().end;
+    const total = this.scrollViewport.getDataLength();
+    if (end === total){
+      this.loadData();
+    }
+  }
+
+  trackByFn(index: number, item: any): number {
+    return item.id;
   }
 }
